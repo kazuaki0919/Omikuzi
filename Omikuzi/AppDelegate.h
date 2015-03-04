b11db67291eb9d0016d7a5936674000b6fba4339
並木一晃 <KAZUAKI@namikikazuaki-no-MacBook-Air.local>
2015-03-04 19:28:48 +0900

    Initial Commit

---
+++ Omikuzi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Omikuzi
+//
+//  Created by 並木一晃 on 2015/03/04.
+//  Copyright (c) 2015年 並木一晃. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Omikuzi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Omikuzi
+//
+//  Created by 並木一晃 on 2015/03/04.
+//  Copyright (c) 2015年 並木一晃. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
